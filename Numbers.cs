1a09c773b43784b7d70fc20d2e149af6c28654c0
fajarkur219 <fajarkur219@student.amikom.ac.id>
2022-04-07 10:30:43 +0700

    OverloadingMethod

---
+++ Numbers.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            int min;
+            if (number1 < number2)
+            {
+                min = number1;
+            }
+            else
+            {
+                min = number2;
+            }
+            return min;
+        }
+
+        public int FindMinimum(int number1, int number2, int number3)
+        {
+
+            int[] nilai = { number1, number2, number3 };
+            int min = nilai[0];
+            for (int i = 0; i < 3; i++)
+            {
+                if (nilai[i] < min)
+                {
+                    min = nilai[i];
+                }
+            }
+
+
+            return min;
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            int min;
+            if (number1 > number2)
+            {
+                min = number1;
+            }
+            else
+            {
+                min = number2;
+            }
+            return min;
+        }
+
+        public int FindMaximum(int number1, int number2, int number3)
+        {
+
+            int[] nilai = { number1, number2, number3 };
+            int min = nilai[0];
+            for (int i = 0; i < 3; i++)
+            {
+                if (nilai[i] > min)
+                {
+                    min = nilai[i];
+                }
+            }
+
+
+            return min;
+        }
+
+    }
+}
